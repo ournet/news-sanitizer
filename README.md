98b6e6c4c78575a6b9d2540ac2684403a635dd0b
Dumitru Cantea <canteadumitru@gmail.com>
2018-09-05 09:49:11 +0300

    changed sanitizeTitle alg

--- README.md
+++ README.md
@@ -1,3 +1,17 @@
 # news-sanitizer
 
 Sanitize news title and body.
+
+## Usage
+
+```ts
+
+import { sanitizeTitle } from 'news-sanitizer';
+
+const lang = 'ro';
+const title = '(VIDEO) Alegerile parlamentare au avot loc cu incalcari';
+
+const newTitle = sanitizeTitle(title, lang);
+// newTitle = "Alegerile parlamentare au avot loc cu incalcari"
+
+```
--- src/sanitize-title.ts
+++ src/sanitize-title.ts
@@ -6,7 +6,7 @@ export function sanitizeTitle(title: string, lang: string) {
     }
 
     if (CACHE[lang] === undefined) {
-        CACHE[lang] = getRegExp(lang);
+        CACHE[lang] = getItem(lang);
     }
 
     const item = CACHE[lang];
@@ -15,43 +15,99 @@ export function sanitizeTitle(title: string, lang: string) {
         return title;
     }
 
-    let regRezult = item.start.exec(title);
-    if (regRezult) {
-        const index = regRezult.index + regRezult[0].length;
-        if (index <= item.maxLine) {
-            title = title.substr(index);
-        }
-    }
-    regRezult = item.end.exec(title);
-    if (regRezult) {
-        const index = regRezult.index;
-        if (title.length - index <= item.maxLine) {
-            title = title.substr(0, index);
-        }
-    }
+    title = sanitizeTitleStart(title, item);
+    title = sanitizeTitleEnd(title, item);
 
     return title;
 }
 
-function getRegExp(lang: string) {
+function sanitizeTitleEnd(title: string, data: DataItem) {
+    const parts = title.split(END_SEPARATOR);
+
+    if (parts.length < 2) {
+        return title;
+    }
+
+    const maxLength = data.maxWordLength * 2;
+    const segments: string[] = []
+    let segmentLength = 0;
+
+    for (let i = parts.length - 1; i >= 0; i--) {
+        const part = parts[i];
+        const l = segmentLength + part.length + 1;
+
+        if (l < maxLength && l < title.length / 1.5) {
+            segments.push(part);
+            segmentLength += part.length + 1;
+        } else {
+            break;
+        }
+    }
+    if (!segmentLength) {
+        return title;
+    }
+    const segment = segments.reverse().join(' ');
+
+    const regResult = data.reg.exec(segment);
+
+    if (!regResult) {
+        return title;
+    }
+
+    title = title.substr(0, title.length - segment.length - 1).trim();
+
+    return title;
+}
+
+function sanitizeTitleStart(title: string, data: DataItem) {
+    const parts = title.split(START_SEPARATOR);
+
+    if (parts.length < 2) {
+        return title;
+    }
+
+    const maxLength = data.maxWordLength * 2;
+    let segment = '';
+    for (const part of parts) {
+        const l = segment.length + part.length + 1;
+        if (l < maxLength && l < title.length / 1.5) {
+            segment = segment ? (segment + ' ' + part) : part;
+        } else {
+            break;
+        }
+    }
+    if (!segment) {
+        return title;
+    }
+
+    const regResult = data.reg.exec(segment);
+
+    if (!regResult) {
+        return title;
+    }
+
+    title = title.substr(segment.length + 1).trim();
+
+    return title;
+}
+
+function getItem(lang: string) {
     const lines = (readTitles(lang) || []).map(escapeRegExp);
     if (!lines.length) {
         return null;
     }
     return {
-        start: new RegExp('\\b(' + lines.join('|') + ')\\s*' + START_SEPARATOR + '\\s*', 'i'),
-        end: new RegExp('\\s*' + END_SEPARATOR + '\\s*(' + lines.join('|') + ')\\b', 'i'),
-        maxLine: lines.reduce<number>((max, current) => current.length > max ? current.length : max, 0) + 3,
+        reg: new RegExp('\\b(' + lines.join('|') + ')\\b', 'i'),
+        maxWordLength: lines.reduce<number>((max, current) => current.length > max ? current.length : max, 0),
     }
 }
 
-const START_SEPARATOR = '[' + escapeRegExp(':|/)-].') + ']';
-const END_SEPARATOR = '[' + escapeRegExp(':|/(-[.') + ']';
+const START_SEPARATOR = new RegExp('[' + escapeRegExp(':|/)-].') + ']', 'g');
+const END_SEPARATOR = new RegExp('[' + escapeRegExp(':|/(-[.') + ']', 'g');
 
 const CACHE: { [key: string]: DataItem | null } = {};
 
 type DataItem = {
-    start: RegExp
-    end: RegExp
-    maxLine: number
+    reg: RegExp
+    maxWordLength: number
 }
